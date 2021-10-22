d0bb25815dfe62fd909de19f3a114c9433d3fa0d
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-22 13:10:06 +0800

    Update Linux Shell.sh

--- Linux Shell.sh
+++ Linux Shell.sh
@@ -6,3 +6,31 @@ echo HELLO
 #!/bin/bash
 read name
 echo "Welcome ${name}"
+
+#Looping with Numbers
+#!/bin/bash
+for i in {1..50}
+do
+    echo $i
+done
+
+#The World of Numbers
+#!/bin/bash
+read X
+read Y
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
+
+#Comparing Numbers
+#!/bin/bash
+read X 
+read Y
+if (( $X < $Y )); then
+    echo 'X is less than Y'
+elif (( $X > $Y )); then
+    echo 'X is greater than Y'
+else
+    echo 'X is equal to Y'
+fi
