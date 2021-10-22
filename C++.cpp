3080df8b857747ad6421ad9e11426a9cb690a05d
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-22 22:36:22 +0800

    Create C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,38 @@
+#define tag 3
+
+#if tag == 1
+//Say "Hello, World!" With C++
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+#elif tag == 2
+//Input and Output
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
+
+#elif tag == 3
+//Conditional Statements
+
+
+
+
+
+#endif
