4465f0ef263befa606d8e66fdd38abf59987a5c0
Hung An Kuo <andykuo8766@gapp.nthu.edu.tw>
2021-10-31 23:59:25 +0800

    Update Linux Shell.sh

--- Linux Shell.sh
+++ Linux Shell.sh
@@ -1,20 +1,20 @@
-#Let's Echo
+# Let's Echo
 #!/bin/bash
 echo HELLO
 
-#A Personalized Echo
+# A Personalized Echo
 #!/bin/bash
 read name
 echo "Welcome ${name}"
 
-#Looping with Numbers
+# Looping with Numbers
 #!/bin/bash
 for i in {1..50}
 do
     echo $i
 done
 
-#The World of Numbers
+# The World of Numbers
 #!/bin/bash
 read X
 read Y
@@ -23,7 +23,7 @@ echo $((X-Y))
 echo $((X*Y))
 echo $((X/Y))
 
-#Comparing Numbers
+# Comparing Numbers
 #!/bin/bash
 read X 
 read Y
@@ -33,4 +33,13 @@ elif (( $X > $Y )); then
     echo 'X is greater than Y'
 else
     echo 'X is equal to Y'
+fi
+
+# Getting started with conditionals
+read answer
+
+if [[ ${answer,,}  == 'y' ]] ; then
+    echo 'YES'
+elif [[ ${answer,,} == 'n' ]] ; then
+    echo 'NO'
 fi
