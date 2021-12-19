383a892bd1772d1d9a6a6dfc0126d914570b06f9
andykuo8766 <andykuo8766@gapp.nthu.edu.tw>
2021-12-19 07:34:47 -0800

    Update Linux Shell

--- Linux_Shell.sh
+++ Linux_Shell.sh
@@ -92,3 +92,40 @@ while read -r line || [[ -n "$line" ]]; do
     sum=$(($sum + $line))
 done
 printf "%.3f" $(echo "scale=10; $sum/$n" | bc -l)
+
+# Functions and Fractals - Recursive Trees - Bash!
+#!/bin/bash
+
+declare -A a
+
+# d - depth
+# l = length
+# r = row
+# c = column
+
+f() {
+    local d=$1 l=$2 r=$3 c=$4
+    [[ $d -eq 0 ]] && return
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$c]=1
+    done
+    ((r -= l))
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$((c-i))]=1
+        a[$((r-i)).$((c+i))]=1
+    done
+    f $((d-1)) $((l/2)) $((r-l)) $((c-l))
+    f $((d-1)) $((l/2)) $((r-l)) $((c+l))
+}
+read n
+f $n 16 63 49
+for ((i=0; i<63; i++)); do
+    for ((j=0; j<100; j++)); do
+        if [[ ${a[$i.$j]} ]]; then
+            printf 1
+        else
+            printf _
+        fi
+    done
+    echo
+done
